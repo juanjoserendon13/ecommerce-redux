7b98e21076fc83475817bb21e570c5d752f8cf8d
Juan Jose Rendon <juanjoserendon13@outlook.com>
2018-08-23 16:20:37 -0500

    redux and router configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Route } from "react-router";
+import { ProductListContainer } from './components/ProductListContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <header>Ecommerce</header>
+        <Route path="/" component={ProductListContainer} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route } from 'react-router';
+import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
+import { syncHistoryWithStore } from 'react-router-redux';
+import 'babel-polyfill';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import configureStore from './store/configStore';
+import initialState from './reducers/initialState';
+
+import App from './App';
+const store = configureStore(initialState);
+const history = syncHistoryWithStore(createBrowserHistory(), store);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={history}>
+            <Route component={App} />
+        </Router>
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
